5b4705d7dec33b8b90590dada07d22e09b322750
Dhruv Segat <dhruv.segat@matillion.com>
2020-01-06 17:40:42 +0530

    Details table implementation

---
+++ src/components/stockDetailsTable.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Table } from 'react-bootstrap';
+import { useTable, useResizeColumns } from 'react-table';
+
+
+const StockDetailsTable = ({ columnData, tableData }) => {
+    const columns = React.useMemo(() => columnData, [columnData]);
+    const data = React.useMemo(() => tableData, [tableData]);
+    const defaultColumn = React.useMemo(
+        () => ({
+            minWidth: 30,
+            width: 150,
+            maxWidth: 400,
+        }),
+        []
+    )
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow
+    } = useTable({
+        columns,
+        data,
+        defaultColumn
+    }, useResizeColumns);
+
+    return (
+        <Table bordered striped hover {...getTableProps()}>
+            <thead>
+                {headerGroups.map(headerGroup => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map(column => (
+                            <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                        ))}
+                    </tr>
+                ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {rows.map((row, i) => {
+                    prepareRow(row);
+                    return (
+                        <tr {...row.getRowProps()}>
+                            {row.cells.map(cell => {
+                                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                            })}
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </Table>
+    );
+};
+
+export default StockDetailsTable;
--- src/pages/stockDetails.js
+++ src/pages/stockDetails.js
@@ -1,10 +1,53 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import StockDetailsTable from '../components/stockDetailsTable';
 
 
-function StockDetails({ match }) {
-    const [stockDetails, setStockDetails] = useState({
-        data: []
-    });
+const createColumnsArray = (groupData, history) => {
+    console.log(`Creating columns array from ${JSON.stringify(groupData)}`);
+    const columns = [
+        {
+            Header: 'Indicator Type',
+            accessor: 'type'
+        },
+        {
+            Header: 'Time Period',
+            accessor: 'period'
+        },
+        {
+            Header: 'Indicator',
+            accessor: 'indicator'
+        },
+        {
+            Header: 'Value',
+            accessor: 'value'
+        },
+        {
+            Header: 'Comments',
+            accessor: 'comment'
+        },
+        {
+            Header: 'Date Ref',
+            accessor: 'dateRef'
+        }
+    ];
+    let header = '';
+    if (groupData && groupData.groupName) {
+        header = groupData.groupName;
+    }
+
+    return [
+        {
+            Header: header,
+            columns: columns
+        }
+    ];
+};
+
+
+const StockDetails = ({ match }) => {
+    let history = useHistory();
+    const [stockDetails, setStockDetails] = useState({});
 
     useEffect(() => {
         const entryId = match.params.id;
@@ -24,19 +67,36 @@ function StockDetails({ match }) {
             if (!stockDataRequest.ok) {
                 throw Error(stockDataRequest.statusText);
             }
-                
+
             const stockData = await stockDataRequest.json();
             return stockData;
         } catch (error) {
             console.log(`Error occured while fetching stock data: ${error}`);
             return null;
         }
-    }
+    };
 
     return (
         <div>
-            <h1>{`Details page: ${match.params.id}`}</h1>
-            <p>{JSON.stringify(stockDetails)}</p>
+            <h1>{`Details for ${stockDetails.stockName}`}</h1>
+            {
+                stockDetails && stockDetails.indicatorGroups ? (
+                    <>
+                        {stockDetails.indicatorGroups.map(groupData => {
+                            const columns = createColumnsArray(groupData, history);
+                            return (
+                                <StockDetailsTable columnData={columns} tableData={groupData.indicators} />
+                            );
+                        })}
+                    </>
+                ) : (
+                        <>
+                            <h4>Connot find group data.</h4>
+                            <p>{JSON.stringify(stockDetails)}</p>
+                            <h5>{`Condition: ${stockDetails && stockDetails.indicatorGroups}`}</h5>
+                        </>
+                    )
+            }
         </div>
     );
 }
