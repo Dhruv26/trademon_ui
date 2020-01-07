69dcc13143bce3c54d5a3e0d5dc70def5b7aaeb5
Dhruv Segat <dhruv.segat@matillion.com>
2020-01-07 16:50:08 +0530

    Move AddPage to Funtional Comp

--- src/pages/addStockEntry.js
+++ src/pages/addStockEntry.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import AddStockEntryForm from '../components/addStockEntryForm'
 
 
-class AddPage extends Component {
-    constructor() {
-        super();
-        this.state = {
-            indicators: [],
-            indicatorTypeOptions: [],
-            timePeriods: [],
-        };
-    }
+const AddPage = () => {
+    const [formOptions, setFormOptions] = useState({
+        indicators: [],
+        indicatorTypeOptions: [],
+        timePeriods: [],
+    });
 
-    async componentDidMount() {
-        const metadata = await this.getParameterTypeOptions();
+    useEffect(() => {
+        async function setOptions() {
+            const metadata = await getParameterTypeOptions();
+            setFormOptions({
+                indicators: metadata.indicators,
+                indicatorTypeOptions: metadata.indicatorTypes,
+                timePeriods: metadata.timePeriods,
+            });
+        }
+        setOptions();
+    }, []);
 
-        this.setState({
-            indicators: metadata.indicators,
-            indicatorTypeOptions: metadata.indicatorTypes,
-            timePeriods: metadata.timePeriods,
-        });
-    }
-
-    async getParameterTypeOptions() {
+    const getParameterTypeOptions = async () => {
         try {
             const response = await fetch('http://localhost:5000/get_entry_meta_data');
             if (!response.ok) {
@@ -40,23 +39,21 @@ class AddPage extends Component {
         }
     }
 
-    render() {
-        return (
-            <div className="container">
-                <div className="row mb-2">
-                    <div className="col-lg-12 text-center">
-                        <h1 className="mt-2">Add Stock Entry</h1>
-                    </div>
+    return (
+        <div className="container">
+            <div className="row mb-2">
+                <div className="col-lg-12 text-center">
+                    <h1 className="mt-2">Add Stock Entry</h1>
                 </div>
-                <h3>Please enter the following details</h3>
-                <AddStockEntryForm
-                    indicatorOptions={this.state.indicators}
-                    indicatorTypeOptions={this.state.indicatorTypeOptions}
-                    timePeriods={this.state.timePeriods}
-                />
             </div>
-        );
-    }
+            <h3>Please enter the following details</h3>
+            <AddStockEntryForm
+                indicatorOptions={formOptions.indicators}
+                indicatorTypeOptions={formOptions.indicatorTypeOptions}
+                timePeriods={formOptions.timePeriods}
+            />
+        </div>
+    );
 }
 
 export default AddPage;
