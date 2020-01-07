9fff7329ac902bf0c6ce126e6364448d818c6ebe
Dhruv Segat <dhruv.segat@matillion.com>
2020-01-07 17:32:44 +0530

    Add entry creation success message

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,96 +1,105 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import IndexTable from '../components/indexPageTable';
-import { Button } from 'react-bootstrap';
+import { Button, Alert } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 
 
 const ID_KEY = '_id';
 
+const getTableData = async () => {
+    try {
+        const tableDataResponse = await fetch('http://localhost:5000/get_table');
+        if (!tableDataResponse.ok) {
+            throw Error(tableDataResponse.statusText);
+        }
 
-class HomePage extends Component {
-    state = {
+        const tableData = await tableDataResponse.json();
+        return tableData;
+    } catch (error) {
+        return {
+            Data: [],
+        };
+    }
+}
+
+
+const HomePage = (props) => {
+    const [tableData, setTableData] = useState({
         columns: [],
         data: []
-    };
+    });
+    const [showCreationSuccess, setCreationSuccess] = useState(false);
+    let history = useHistory();
 
-    async handleMoreInfo(rowData) {
+    const handleMoreInfo = async (rowData) => {
         console.log(rowData);
         const currID = rowData[ID_KEY];
-        let history = this.props.history;
         history.push(`/details/${currID}`);
     };
 
-    async componentDidMount() {
-        const columns = [{
-            Header: 'My Table',
-            columns: [
-                {
-                    Header: 'Stock Name',
-                    accessor: 'StockName'
-                },
-                {
-                    Header: 'EMA',
-                    accessor: 'EMA'
-                },
-                {
-                    Header: 'SMA',
-                    accessor: 'SMA'
-                },
-                {
-                    Header: 'RSI',
-                    accessor: 'RSI'
-                },
-                {
-                    Header: 'More Info',
-                    Cell: cellInfo => (
-                        <div>
-                            <Button size="sm" onClick={() => {
-                                const index = cellInfo.row.index;
-                                this.handleMoreInfo(cellInfo.data[index]);
-                            }}>
-                                More Info
-                            </Button>
-                        </div>
-                    )
-                }
-            ]
-        }];
-
-        const tableData = await this.getTableData();
-        const data = tableData.Data;
-
-        this.setState({
-            columns: columns,
-            data: data
-        });
-    }
-
-    async getTableData() {
-        try {
-            const tableDataResponse = await fetch('http://localhost:5000/get_table');
-            if (!tableDataResponse.ok) {
-                throw Error(tableDataResponse.statusText);
-            }
-
-            const tableData = await tableDataResponse.json();
-            return tableData;
-        } catch (error) {
-            return {
-                Data: [],
-            };
+    useEffect(() => {
+        async function setTableDataState() {
+            const columns = [{
+                Header: 'My Table',
+                columns: [
+                    {
+                        Header: 'Stock Name',
+                        accessor: 'StockName'
+                    },
+                    {
+                        Header: 'EMA',
+                        accessor: 'EMA'
+                    },
+                    {
+                        Header: 'SMA',
+                        accessor: 'SMA'
+                    },
+                    {
+                        Header: 'RSI',
+                        accessor: 'RSI'
+                    },
+                    {
+                        Header: 'More Info',
+                        Cell: cellInfo => (
+                            <div>
+                                <Button size="sm" onClick={() => {
+                                    const index = cellInfo.row.index;
+                                    handleMoreInfo(cellInfo.data[index]);
+                                }}>
+                                    More Info
+                                </Button>
+                            </div>
+                        )
+                    }
+                ]
+            }];
+    
+            const tableData = await getTableData();
+            const data = tableData.Data;
+    
+            setTableData({
+                columns: columns,
+                data: data
+            });
         }
-    }
+        setTableDataState();
 
-    render() {
-        return (
-            <div>
-                <br></br>
-                <IndexTable
-                    columnData={this.state.columns}
-                    tableData={this.state.data}
-                />
-            </div>
-        );
-    }
+        if (props.location.state && props.location.state.entryCreated) {
+            setCreationSuccess(props.location.state.entryCreated);
+        }
+    }, [props]);
+
+    return (
+        <div>
+            <Alert show={showCreationSuccess} onClose={() => setCreationSuccess(false)} variant="success" className="mt-2 mb-2" dismissible>
+                <p>Entry created successfully!</p>
+            </Alert>
+            <IndexTable
+                columnData={tableData.columns}
+                tableData={tableData.data}
+            />
+        </div>
+    );
 }
 
 export default HomePage;
